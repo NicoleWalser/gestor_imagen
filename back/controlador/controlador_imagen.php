66b64cfc00b70b5c24e02439b7573b30e0284db9
NicoleWalser <IAEnicole05@gmail.com>
2024-06-10 17:55:01 -0300

    avance

--- back/controlador/controlador_imagen.php
+++ back/controlador/controlador_imagen.php
@@ -9,16 +9,15 @@ switch ($function) {
     case "agregar":
       agregarImagen();
     break;
-
+    }
+    
     function agregarImagen(){
         $nombre = $_POST['nombre'];
         
-        $extension = pathinfo(($_FILES)['ima']['neme'], PATHINFO_EXTENSION);
-        move_uploaded_file($_FILES['ima']['name',$extension]);
-        $resultado = (new libro())->agregarImagen( $nombre);
+       
+        $resultado = (new imagen())->agregarImagen( $nombre);
         echo json_encode($resultado);
      }
     
 
 
-}
--- back/modelo/modelo_imagen.php
+++ back/modelo/modelo_imagen.php
@@ -2,10 +2,18 @@
 require_once  '../conexion/conexion.php';
 
 class imagen {
-    public function agregarImagen( $nombre,){
-            $sql = "INSERT INTO  VALUES(0,)";
+    public function agregarImagen($nombre){
+        $extension = pathinfo(($_FILES)['imagen']['neme'], PATHINFO_EXTENSION);
+        $sql = "INSERT INTO  VALUES imagen (0, $nombre, $extension)";
             $connection = connection();
             $respuesta = $connection->query($sql);
+           $id = $connection-> insert_id;
+            
+            move_uploaded_file($_FILES['imagen']['tmp_name'],'./img/'.$id.$extension);
             return $respuesta;
+           
     }
+
+
 }
+
